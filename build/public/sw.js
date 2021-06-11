66fadbdabb5f14159a7823846999053f0c48c936
moringaman <rob@webnostix.co.uk>
2021-06-12 00:01:47 +0100

    add prepush git commands

--- build/public/sw.js
+++ build/public/sw.js
@@ -15,7 +15,7 @@ importScripts("/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v3.6.3"});
 
 importScripts(
-  "/precache-manifest.cb4fb8f4861d043ce7554dc0bc5c5e21.js"
+  "/precache-manifest.ad5e026c938b1bff360515f2fa6ca54f.js"
 );
 
 workbox.skipWaiting();
