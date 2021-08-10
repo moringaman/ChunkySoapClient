cf088e2c89d996033f7fb6e5a176aaa19bc0a7de
moringaman <rob@webnostix.co.uk>
2021-08-10 16:41:07 +0100

    [BUILD]: Pre-push build step

--- build/public/precache-manifest.991a89065f826efa1f89f80faf3d473c.js
+++ build/public/precache-manifest.991a89065f826efa1f89f80faf3d473c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "92b5312f4fa3e58f8127",
+    "revision": "4897e0251c521243ea6c",
     "url": "/static/css/bundle.2d80b716.css"
   },
   {
-    "revision": "92b5312f4fa3e58f8127",
-    "url": "/static/js/bundle.92b5312f.js"
+    "revision": "4897e0251c521243ea6c",
+    "url": "/static/js/bundle.4897e025.js"
   },
   {
     "revision": "b9ddca6af362f818465d",
@@ -33,7 +33,7 @@ self.__precacheManifest = [
   },
   {
     "revision": "22d277286562c0b9ceb638a94204e916",
-    "url": "/static/js/bundle.92b5312f.js.LICENSE.txt"
+    "url": "/static/js/bundle.4897e025.js.LICENSE.txt"
   },
   {
     "url": "/icon_180x180.8e34bc6d815173fe01ec33c77c5f660f.png"
--- build/public/sw.js
+++ build/public/sw.js
@@ -15,7 +15,7 @@ importScripts("/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v3.6.3"});
 
 importScripts(
-  "/precache-manifest.58247e38f922c5728e9af07a51751430.js"
+  "/precache-manifest.991a89065f826efa1f89f80faf3d473c.js"
 );
 
 workbox.skipWaiting();
