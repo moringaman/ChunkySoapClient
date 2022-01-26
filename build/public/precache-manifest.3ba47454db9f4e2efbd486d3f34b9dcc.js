4458d9c012a144cce9f60f1df85ce97445b0344d
moringaman <rob@webnostix.co.uk>
2022-01-26 14:57:10 +0000

    [BUILD]: Pre-push build step

---
+++ build/public/precache-manifest.3ba47454db9f4e2efbd486d3f34b9dcc.js
@@ -0,0 +1,83 @@
+self.__precacheManifest = [
+  {
+    "revision": "faad7e27f2f9cf0fdc48",
+    "url": "/static/css/bundle.2d80b716.css"
+  },
+  {
+    "revision": "faad7e27f2f9cf0fdc48",
+    "url": "/static/js/bundle.faad7e27.js"
+  },
+  {
+    "revision": "b9ddca6af362f818465d",
+    "url": "/static/js/1.b9ddca6a.chunk.js"
+  },
+  {
+    "revision": "5784441e684d7cac0113",
+    "url": "/static/js/2.5784441e.chunk.js"
+  },
+  {
+    "revision": "561ebf46b17f286f41a5",
+    "url": "/static/js/3.561ebf46.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/static/js/2.5784441e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385c9a659c8a5182b86a7090d58d0fbc",
+    "url": "/static/js/3.561ebf46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/1.b9ddca6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d277286562c0b9ceb638a94204e916",
+    "url": "/static/js/bundle.faad7e27.js.LICENSE.txt"
+  },
+  {
+    "url": "/icon_180x180.8e34bc6d815173fe01ec33c77c5f660f.png"
+  },
+  {
+    "url": "/icon_152x152.ca9cd38e55fddd88570518e8c0299f23.png"
+  },
+  {
+    "url": "/icon_144x144.1643b60b2714dd369e453ca884897666.png"
+  },
+  {
+    "url": "/icon_120x120.715eb318ff340e017ccca08c2db15cb8.png"
+  },
+  {
+    "url": "/icon_114x114.dd54050e25b8bb58d0e2433dc2995cad.png"
+  },
+  {
+    "url": "/icon_76x76.22d7b8a972a188f00270debd7f411358.png"
+  },
+  {
+    "url": "/icon_72x72.f2d5307ca029e33b222a21552a08a109.png"
+  },
+  {
+    "url": "/icon_60x60.754cd5221c4be7447b6f4d2d4a5b5e18.png"
+  },
+  {
+    "url": "/icon_57x57.0d199f525995ac794fa2af8b5ff15366.png"
+  },
+  {
+    "url": "/icon_192x192.1dec1dd565712cd97bd9fa828bcb356c.png"
+  },
+  {
+    "url": "/icon_96x96.d79ec2402bdb8483fb8fe9415dbf9d1e.png"
+  },
+  {
+    "url": "/icon_48x48.ee7db214ca94e210a7b9df96b9175c7a.png"
+  },
+  {
+    "url": "/icon_36x36.597833536f5dbb41bee82cd6d5e738cc.png"
+  },
+  {
+    "url": "/icon_32x32.ed670ba65ef48bbcfbd0d9792d944fb5.png"
+  },
+  {
+    "url": "/icon_16x16.7c16fadb7ec472d485127634f14a015e.png"
+  }
+];
